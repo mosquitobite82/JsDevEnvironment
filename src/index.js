c3c326be4f19af47a07a78ce92246cd7a8f9201e
Johannes Lindgren <johannes.lindgren@mosquitobite.se>
2017-01-28 19:15:10 +0100

    eslint-watch with default rules

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import numeral from 'numeral';
 
 const val = numeral(1000).format('$0,0.00');
 
-console.log(`Wow! ${val} dollars!`);
+console.log(`Wow! ${val} dollars!`); // eslint-disable-line no-console
