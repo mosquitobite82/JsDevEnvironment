36b7b28923ae300e0281068bd4e4dc1d88450522
Johannes Lindgren <mosquitobite82@gmail.com>
2020-04-02 22:38:57 +0200

    mockdata_change

--- scripts/mockDataSchema.js
+++ scripts/mockDataSchema.js
@@ -3,26 +3,29 @@ export const schema = {
   "properties": {
     "users": {
       "type": "array",
-      "minItems": 3,
-      "maxItems": 5,
+      "minItems": 20,
+      "maxItems": 35,
       "items": {
         "type": "object",
         "properties": {
           "id": {
-            "type": "number",
+            "type": "integer",
             "unique": true,
             "minimum": 1
           },
           "firstName": {
             "type": "string",
+            "pattern": "^\[A-ZÅÄÖ\]\[a-zåäö\]{2,10}",
             "faker": "name.firstName"
           },
           "lastName": {
             "type": "string",
+            "pattern": "^\[A-ZÅÄÖ\]\(\[a-zåäö\]{3,10}\)\( \[A-ZÅÄÖ\]\[a-zåäö\]{3,10}\){0,2}",
             "faker": "name.lastName",
           },
           "email": {
             "type": "string",
+            "pattern": "^[a-z]{7,12}[0-9]{0,4}@(yahoo|gmail|hotmail)\\.(se|com|uk|de)",
             "faker": "internet.email",
           }
         },
